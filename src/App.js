b76ba6b03d7db90ce2014e4723028b836699b984
Antonio Rodriguez <antoniwan@gmail.com>
2019-06-28 15:46:19 -0400

    Completed the React useReducer, useContext exercise

--- src/App.js
+++ src/App.js
@@ -1,25 +1,145 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useReducer, createContext, useContext } from "react";
+import uuid from "uuid/v4";
+import filterReducer from "./reducers/filterReducer";
+import todoReducer from "./reducers/todoReducer";
+
+const TodoContext = createContext(null);
+const FilterContext = createContext(null);
+
+const initialTodos = [
+  {
+    id: uuid(),
+    task: "Learn React",
+    complete: true
+  },
+  {
+    id: uuid(),
+    task: "Learn Firebase",
+    complete: true
+  },
+  {
+    id: uuid(),
+    task: "Learn GraphQL",
+    complete: false
+  },
+  {
+    id: uuid(),
+    task: "Learn Apolo",
+    complete: false
+  }
+];
+
+const Filter = () => {
+  const dispatch = useContext(FilterContext);
+  const handleShowAll = () => {
+    dispatch({ type: "SHOW_ALL" });
+  };
+  const handleShowComplete = () => {
+    dispatch({ type: "SHOW_COMPLETE" });
+  };
+  const handleShowIncomplete = () => {
+    dispatch({ type: "SHOW_INCOMPLETE" });
+  };
+
+  return (
+    <div>
+      <button type="button" onClick={handleShowAll}>
+        Show All
+      </button>
+      <button type="button" onClick={handleShowComplete}>
+        Show Complete
+      </button>
+      <button type="button" onClick={handleShowIncomplete}>
+        Show Incomplete
+      </button>
+    </div>
+  );
+};
+
+const TodoList = ({ todos }) => {
+  return (
+    <ul>
+      {todos.map(todo => (
+        <TodoItem key={todo.id} todo={todo} />
+      ))}
+    </ul>
+  );
+};
+
+const TodoItem = ({ todo }) => {
+  const dispatch = useContext(TodoContext);
+  const handleChange = () => {
+    dispatch({
+      type: todo.complete ? "UNDO_TODO" : "COMPLETE_TODO",
+      id: todo.id
+    });
+  };
+
+  return (
+    <li>
+      <label>
+        <input
+          type="checkbox"
+          checked={todo.complete}
+          onChange={handleChange}
+        />
+        {todo.task}
+      </label>
+    </li>
+  );
+};
+
+const AddTodo = () => {
+  const dispatch = useContext(TodoContext);
+  const [task, setTask] = useState("");
+
+  const handleSubmit = event => {
+    if (task) {
+      dispatch({ type: "ADD_TODO", task: task, id: uuid() });
+    }
+
+    setTask("");
+    event.preventDefault();
+  };
+
+  const handleChange = event => setTask(event.target.value);
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input type="text" value={task} onChange={handleChange} />
+      <button type="submit">Add Todo</button>
+    </form>
+  );
+};
 
 function App() {
+  const [todos, dispatchTodos] = useReducer(todoReducer, initialTodos);
+  const [filter, dispatchFilter] = useReducer(filterReducer, "ALL");
+
+  const filteredTodos = todos.filter(todo => {
+    if (filter === "ALL") {
+      return true;
+    }
+
+    if (filter === "COMPLETE" && todo.complete) {
+      return true;
+    }
+
+    if (filter === "INCOMPLETE" && !todo.complete) {
+      return true;
+    }
+
+    return false;
+  });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <TodoContext.Provider value={dispatchTodos}>
+      <FilterContext.Provider value={dispatchFilter}>
+        <Filter />
+        <TodoList todos={filteredTodos} />
+        <AddTodo />
+      </FilterContext.Provider>
+    </TodoContext.Provider>
   );
 }
 
---
+++ src/reducers/filterReducer.js
@@ -0,0 +1,14 @@
+const filterReducer = (_state, action) => {
+  switch (action.type) {
+    case "SHOW_ALL":
+      return "ALL";
+    case "SHOW_COMPLETE":
+      return "COMPLETE";
+    case "SHOW_INCOMPLETE":
+      return "INCOMPLETE";
+    default:
+      throw new Error();
+  }
+};
+
+export default filterReducer;
